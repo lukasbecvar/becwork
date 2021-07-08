e7e3691908c9766d01d3f953134da779c0fa9c3d
Lukáš Bečvář <lordbecvold@gmail.com>
2021-07-08 04:37:23 +0200

    Added encoding config

--- framework/crypt/CryptUtils.php
+++ framework/crypt/CryptUtils.php
@@ -2,10 +2,21 @@
 
     class CryptUtils {
 
+        /*
+         * Base64 gen function
+         * Input: String or file (img, etc.)
+         * Return: Base64 code
+        */
         public function genBase64($string) {
             return base64_encode($string);
         }
 
+
+        /*
+         * Base64 decode function
+         * Input: base64 code
+         * Return: string or file 
+        */
         public function decodeBase64($base64) {
             return base64_decode($base64);
         }
