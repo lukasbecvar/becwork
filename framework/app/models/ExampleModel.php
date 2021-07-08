040a296b8e055adf28df82f892b0768b804ff275
Lukáš Bečvář <lordbecvold@gmail.com>
2021-07-08 21:38:43 +0200

    Added models

---
+++ framework/app/models/ExampleModel.php
@@ -0,0 +1,13 @@
+<?php  //The Example app Model
+
+	class ExampleModel {
+
+
+		public $data = [
+			"name" => "value_name",
+			"password" => "value_password",
+			"online" => false
+		];
+	}
+
+?>
