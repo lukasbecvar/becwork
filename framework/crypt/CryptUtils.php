09aa3c52ae1695ff7969df3313d7da2104836121
lordbecvold <lordbecvold@gmail.com>
2021-11-30 12:58:03 +0100

    Updated crypt utils

--- framework/crypt/CryptUtils.php
+++ framework/crypt/CryptUtils.php
@@ -22,40 +22,23 @@
         }
 
 
-        public function sign($message, $key) {
-            return hash_hmac('sha256', $message, $key) . $message;
-        }
-        
-
-        public function verify($bundle, $key) {
-            return hash_equals(
-              hash_hmac('sha256', mb_substr($bundle, 64, null, '8bit'), $key),
-              mb_substr($bundle, 0, 64, '8bit')
-            );
-        }
-        
-
-        public function getKey($password, $keysize = 16) {
-            return hash_pbkdf2('sha256',$password,'some_token',100000,$keysize,true);
-        }
-        
-
-        public function encrypt($message, $password) {
-            $iv = random_bytes(16);
-            $key = $this->getKey($password);
-            $result = $this->sign(openssl_encrypt($message,'aes-256-ctr',$key,OPENSSL_RAW_DATA,$iv), $key);
-            return bin2hex($iv).bin2hex($result);
+        /*
+          * AES128 image encrypt
+          * Input: Image file, encrypt key
+          * Return: encrypted image
+        */
+        public function encryptImageAES($image, $key) {
+            return openssl_encrypt($image, "aes-128-cbc", $key);
         }
 
-        
-        public function decrypt($hash, $password) {
-            $iv = hex2bin(substr($hash, 0, 32));
-            $data = hex2bin(substr($hash, 32));
-            $key = $this->getKey($password);
-            if (!$this->verify($data, $key)) {
-              return null;
-            }
-            return openssl_decrypt(mb_substr($data, 64, null, '8bit'),'aes-256-ctr',$key,OPENSSL_RAW_DATA,$iv);
+
+        /*
+          * AES128 image decrypt
+          * Input: Image file, decrypt key
+          * Return: decrypted image
+        */
+        public function decryptImageAES($encryptedImage, $key) {
+            return openssl_decrypt($encryptedImage, "aes-128-cbc", $key);
         }
     }
 ?>
