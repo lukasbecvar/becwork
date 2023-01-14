60a566df723d473a5d8bcddd11648fc1cd5f98a5
lukas <lordbecvold@gmail.com>
2023-01-14 10:34:25 +0100

    [+] vendor check in start

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+# color codes.
+red_echo () { echo "$(tput setaf 9)$1"; }
+die() { echo "$*" 1>&2 ; exit 1; }
+
+# check if vendor installed
+if [ ! -d "vendor/" ]
+then
+    red_echo "Build-error: vendor not found, please install composer"
+    die
+fi
+
 clear
 cd public/ 
 sudo php -S localhost:80
