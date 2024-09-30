350d7cce5809622bd46c266ae391abe6195c061a
Lukáš Bečvář <lordbecvold@gmail.com>
2024-09-30 10:29:31 +0200

    fixed usernames in links

--- site/Main.php
+++ site/Main.php
@@ -12,7 +12,7 @@
 	<main>
 		<p class="title-msg"><strong>Becwork</strong></p>
 		<p class="doc-msg">click -> <a href="/example">examples</a></p>
-		<p class="doc-msg">The documentation is available <a href="https://github.com/lordbecvold/becwork/blob/d0401f1b6bdc99996c9009876d1e656d91de95bc/DOC.MD" target="_blank">here</a></p>
+		<p class="doc-msg">The documentation is available <a href="https://github.com/lukasbecvar/becwork/blob/d0401f1b6bdc99996c9009876d1e656d91de95bc/DOC.MD" target="_blank">here</a></p>
 	</main>
 </body>
 </html>
